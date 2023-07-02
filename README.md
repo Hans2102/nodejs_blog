dd440a7819f1cb5ee289849bbe3f7521ab44cbfa
Lê Tùng <tnghan2111@gmail.com>
2023-07-03 01:34:44 +0700

    MVC routes % controller

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 Nodejs fundamental 
--Ínt
+-Day 1
 HTTP protocol
 SSR & CSR{
     Server side rendering(SEO support)
     Client side rendering
 } 
 install express.js, nodemon.js(auto reload), inspector, morgan(check request), handlebars, node-sass
+-Day 2
+Query parameters(https://google.com/search?q=nodejs expressjs&ref=f8&author=tung)
+Form default behaviour
--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+// app.use(morgan('combined'));
 const handlebars = require('express-handlebars');
 
 const app = express();
 const port = 3000;
 
+const route = require('./routes');
+
 app.use(express.static(path.join(__dirname, 'public')));
-//http logger
-app.use(morgan('combined'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 const hbs = handlebars.create({
   extname: '.hbs'
 });
+
 // Configure Handlebars as the view engine 
 app.engine('hbs', hbs.engine); 
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources','views'));
-app.get('/trangchu', (req, res) => {
-  res.render('home');
-});
-app.get('/news', (req, res) => {
-  res.render('news');
-});
+
+route(app);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 });
