9e879d96d8af9babe03c2cc2608103185a30993b
Lê Tùng <tnghan2111@gmail.com>
2023-07-03 19:07:50 +0700

    test

--- src/index.js
+++ src/index.js
@@ -3,28 +3,26 @@ const express = require('express');
 const morgan = require('morgan');
 // app.use(morgan('combined'));
 const handlebars = require('express-handlebars');
-
 const app = express();
 const port = 3000;
 
 const route = require('./routes');
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 const hbs = handlebars.create({
-  extname: '.hbs'
+    extname: '.hbs',
 });
 
-// Configure Handlebars as the view engine 
-app.engine('hbs', hbs.engine); 
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources','views'));
+// Configure Handlebars as the view engine
+  app.engine('hbs', hbs.engine);
+  app.set('view engine', 'hbs');
+  app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 const newsRouter = require('./news');
 const siteRouter = require('./site');
 
-
 function route(app) {
     // app.get('/news', (req, res) => {
     //     res.render('news');
@@ -9,7 +8,6 @@ function route(app) {
     app.use('/news', newsRouter);
 
     app.use('/', siteRouter);
-
 }
 
 module.exports = route;
